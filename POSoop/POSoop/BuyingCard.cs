7ffe3604868dd12d9c832b59d707edfdf0bea16d
Manjur Ahamed <34239179+Ahamed3619@users.noreply.github.com>
2018-01-05 20:25:13 +0600

    POS Application Using C# OOP
    POS Application Using C# OOP

---
+++ POSoop/POSoop/BuyingCard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSoop
+{
+    public class BuyingCard
+    {
+
+        public Item item;
+       
+    }
+}
